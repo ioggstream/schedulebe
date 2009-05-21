8837b7bdebb3b7f481b5cd5f12a70e78cd2dfafb
robipolli@gmail.com <robipolli@gmail.com@1f0caf80-f1f1-11dd-8c4e-4bbb75a408b7>
2009-05-21 16:23:15 +0000

    tested with request and reply

--- schedulebe.py
+++ schedulebe.py
@@ -19,8 +19,11 @@ import email, httplib
 import re
 import pycurl
 
+
 debug = True
 rtsvcUrl = "http://rtsvc.example.org:28080/pubcaldav/rtsvc"
+rtsvcUrl = "http://caladmin:caladmin@be-spc:8080/pubcaldav/rtsvc"
+
 rtsvcUser = "pippo"
 rtsvcPass = "pluto"
 
@@ -33,7 +36,7 @@ class Meeting():
         self.attendees = []
         
         self.sender = None
-        self.recipient = None
+        self.recipient = []
         
     def setMail(self, mailMessage):
         self.walkMail(mailMessage)
@@ -53,53 +56,65 @@ class Meeting():
                 
         return self.attendees
 
+    def __checkOrganizer(organizer):
+        """check if organizer is an user of the platform"""
+
+        return True
+
     def validate(self):
         
         if self.getMethod() == "REQUEST":
             #mail recipient must be internal and in attendees
             # in a meeting request, organizer is the mail sender
             if self.getOrganizer() != self.sender:
+                print "in %s: Organizer != sender : %s != %s"  % (self.getMethod(), self.getOrganizer(), self.sender)
                 return False
             
-        elif getMethod() == "REPLY":
+        elif self.getMethod() == "REPLY":
             #the sender mail should be one of the attendees
             if not self.sender in self.getAttendees():
+                print "in %s: sender != attendees : %s not contains  %s"  % (self.getMethod(),self.getAttendees(), self.sender)
                 return False
             
         else:
-            return False 
+            print "Error validating meeting"
 
+        return True
         # check that organizer is valid
-        if not __checkOrganizer(self.getOrganizer()):
-            return False
+#        if not __checkOrganizer(self.getOrganizer()):
+#            return False
    
     def walkMail(self, mailMessage):
         """walk thru the mail looking for calendar attachment"""
         """parse attachment and set meeting.ics"""
-        
-        self.recipient = cleanMailAddress(mailMessage['To'])
+
         self.sender = cleanMailAddress(mailMessage['From'])
-    
+        for rcpt in mailMessage['To'].split(","):
+            self.recipient.append(cleanMailAddress(rcpt))
+            
+        if debug:
+            print "DEBUG: from: %s, rcpt: %s" % (mailMessage['From'], mailMessage['To'])
+            
         mailWalker = mailMessage.walk()
         #print "elements: %d" %  countEnum(mailMessage.walk())
-        for i in mailWalker:
+#        for i in mailWalker:
+#                if i.get_content_type() == "text/calendar": 
+#                    if self != None:  
+#                        icalendar = vobject.readOne(i.get_payload(decode=True))
+#                        self.ics = icalendar
+#                    else:
+#                        print "Test case: %s" % i.get_payload(decode=True)
+        try:
+            for i in mailWalker:
                 if i.get_content_type() == "text/calendar": 
                     if self != None:  
                         icalendar = vobject.readOne(i.get_payload(decode=True))
                         self.ics = icalendar
                     else:
                         print "Test case: %s" % i.get_payload(decode=True)
-#        try:
-#            for i in mailWalker:
-#                if i.get_content_type() == "text/calendar": 
-#                    if meeting != None:  
-#                        icalendar = vobject.readOne(i.get_payload(decode=True))
-#                        self.ics = icalendar
-#                    else:
-#                        print "Test case: %s" % i.get_payload(decode=True)
-#        except ParseError:
-#            print "Error while parsing calendar"
-#            raise           
+        except vobject.base.ParseError:
+            print "Error while parsing calendar"
+            raise           
         
          
 #end class
@@ -133,18 +148,21 @@ def sendRequestToBedework(meeting):
     """    Header: recipient:  two@example.com      """
     """    Header: Content-type: text/calendar      """
     """    .ics as POST body                        """
-    rtsvcHeader = [ 'Content-Type:text/calendar; charset=UTF-8' ]
-    rtsvcHeader.append('originator: ' + meeting.getOrganizer())
+    if not meeting.validate():
+        return False
     
-    for a in meeting.getAttendees():
+    rtsvcHeader = [ 'Content-Type:text/calendar; charset=UTF-8' ]
+    rtsvcHeader.append('originator: ' + meeting.sender)
+    
+    for a in set(meeting.getAttendees()).intersection(meeting.recipient):
         rtsvcHeader.append('recipient: ' + a)    
                 
     c = pycurl.Curl()
  
     if debug:
-        print "debug" + meeting.ics.serialize()
+        print "DEBUG:" + meeting.ics.serialize()
         c.setopt(c.VERBOSE, 1)
-        for a in rtsvcHeader: print a
+        for a in rtsvcHeader: print "DEBUG:" + a
 
     c.setopt(c.HTTPHEADER, rtsvcHeader)   
     c.setopt(c.POSTFIELDS, meeting.ics.serialize())
@@ -161,10 +179,7 @@ def __notifyUpdate(isError):
     
     return True
 
-def __checkOrganizer(organizer):
-    """check if organizer is an user of the platform"""
 
-    return True
 
 def countEnum(i):
     tot=0
